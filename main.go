4113adc2cb3b26200ef494e49775b9fd8f771d7c
Leonardus Fernando <leociffer@gmail.com>
2020-03-18 21:47:17 +0700

    godep save

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "os"
+    // "os"
 	"fmt"
 	"net/http"
 	"html/template"
@@ -10,36 +10,39 @@ import (
 type M map[string]interface{}
 
 func main() {
-    port := os.Getenv("PORT")
+    // port := os.Getenv("PORT")
     http.Handle("/static/", 
     	http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
 
-    // http.HandleFunc("/", home)
+    http.HandleFunc("/", home)
     http.HandleFunc("/index", home)
-    fmt.Println("server started at localhost:"+port)
-    http.ListenAndServe(":"+port, nil)
+    
+    fmt.Println("server started at localhost:8080")
+    http.ListenAndServe(":8080", nil)
+    // fmt.Println("server started at localhost:"+port)
+    // http.ListenAndServe(":"+port, nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request){
-    // if r.URL.Path != "/index" && r.URL.Path != "/" {
-    //     errorHandler(w, r, http.StatusNotFound, r.URL.Path)
-    //     return
-    // }
+    if r.URL.Path != "/index" && r.URL.Path != "/" {
+        errorHandler(w, r, http.StatusNotFound, r.URL.Path)
+        return
+    }
     var tmpl = template.Must(template.ParseFiles(
         "views/index.html",
         "views/_header.html",
         "views/_footer.html",
     ))
-	var data = M{"name": "Batman"}
+	var data = M{"about": "Saya Leonardus Fernando Purba, umur masih muda XD, pernah kuliah (lulus walaupun telat), seorang web developer yang berkerja di salah satu perusahaan IT di Pontianak, bahasa pemrograman yang sering digunakan yaitu PHP, JavaScript, Go, dan MySql. Website ini adalah salah satu yang dikembangkan dengan Go."}
     err := tmpl.ExecuteTemplate(w, "index", data)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 }
 
-// func errorHandler(w http.ResponseWriter, r *http.Request, status int, url_path string) {
-//     w.WriteHeader(status)
-//     if status == http.StatusNotFound {
-//         fmt.Fprint(w, "404 page not found. ", url_path)
-//     }
-// }
+func errorHandler(w http.ResponseWriter, r *http.Request, status int, url_path string) {
+    w.WriteHeader(status)
+    if status == http.StatusNotFound {
+        fmt.Fprint(w, "404 page not found bro. ", url_path)
+    }
+}
